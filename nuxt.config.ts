6b4d29fe5d098d81f489ad7b4a8aea4066b3e591
wzc520pyfm <1528857653@qq.com>
2022-11-08 22:09:26 +0800

    build: 配置antd按需引入、自动导入、自动脱ref

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  experimental: {
+    reactivityTransform: true, // 开启自动响应式转换(使用: let age = $ref(1); age = 2// 注意不需要是age.value = 2)
+  },
+  vite: {
+    plugins: [
+      Components({
+        resolvers: [AntDesignVueResolver()], // AntDesign按需引入
+      }),
+    ]
+  }
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,20 @@
 <template>
-  <div class="home">
+  <div class="home" @click="handleClick">
     首页
-    <Banner />
-    <BannerChild />
-    <nuxt-link to="/about?data='我是首页的数据'">跳转去关于页面</nuxt-link>
-    <br />
-    <nuxt-link to="/about/personal">跳转去个人信息</nuxt-link>
-    <br />
-    <div>请求来的数据:{{ data }}</div>
+    <h1>{{ xd }}</h1>
+    <h1>{{ data.a }}</h1>
+    <a-button type="primary">按钮</a-button>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 
-const data = ref('')
+let xd = $ref(1)
+const data = reactive({ a: '小滴课堂' })
 
-const resData = await useFetch('http://127.0.0.1:8081/test', {
-  method: 'GET',
-  params: { id: 1 }
-})
-data.value = resData.data
+const handleClick = () => {
+  xd = 2
+}
 
 </script>
 
