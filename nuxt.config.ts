362c837043a1cd2c42d49839120c2b7b38a777bb
wzc520pyfm <1528857653@qq.com>
2022-11-09 09:21:31 +0800

    build: 配置pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@unocss/nuxt'],
+  modules: ['@unocss/nuxt', '@pinia/nuxt'],
   experimental: {
     reactivityTransform: true, // 开启自动响应式转换(使用: let age = $ref(1); age = 2// 注意不需要是age.value = 2)
   },
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
   <div color-red bg-blue font-600 fs-30 w-100 h-100 flexc>HelloWorld</div>
   <a-button w-90px color-green! bg-red!>按钮</a-button>
+  <h1>pinia初始化值{{ count }}</h1>
+  <a-button @click="addClick">增加</a-button>
+  <h2>当前pinia的getter值: {{ multiplyCount }}</h2>
 </template>
 
 <script setup>
 
+const { count, add, multiplyCount } = $(useUser())
+const addClick = () => add()
 </script>
 
