09a21c4e5df6d5c7e2860c5880018f5dc549c0b4
wzc520pyfm <1528857653@qq.com>
2022-12-12 22:17:07 +0800

    feat：增加轮播图页面布局

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,50 @@
-<template>
-  <div class="home"></div>
-</template>
-
 <script lang="ts" setup>
+import Banner from './index/Banner.vue'
+import Classify from './index/Classify.vue'
+import UserInformation from './index/UserInformation.vue'
 
+useHead({
+  title: '首页'
+})
 </script>
 
+<template>
+  <div style="width: 1200px; min-width: 1200px">
+    <div class="main">
+      <!-- 分类 -->
+      <div class="menu">
+        <Classify />
+      </div>
+      <!-- 轮播图 -->
+      <div class="swiper">
+        <Banner />
+      </div>
+      <!-- 个人信息 -->
+      <div class="user">
+        <UserInformation />
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style lang="less" scoped>
+  .main {
+    display: flex;
+    height: 400px;
+    .swiper {
+      flex: 5;
+      border-radius: 10px;
+    }
+  }
+
+  .card-container {
+    margin-bottom: 20px;
+  }
+
+  .two-card-container {
+    display: flex;
+    margin-bottom: 20px;
+    justify-content: space-between;
+  }
+</style>
--- types/api.d.ts
+++ types/api.d.ts
@@ -6,7 +6,24 @@ export interface IApiBase<T> {
   msg: string;
   data: T;
 }
+/**
+ * 微信二维码接口返回参数
+ */
 export interface IWechat {
   qrcodeUrl: string;
   ticket:    string;
 }
+/**
+ * 课程分类接口返回参数
+ */
+ export interface ICategoryList {
+  id:number;
+  name:string;
+  pid:number;
+  gmt_create:string;
+  gmt_modified:string;
+  level:null;
+  subCategoryList?: ICategoryList[];
+}
+
+
